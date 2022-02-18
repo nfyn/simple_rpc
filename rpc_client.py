2969c18330198eaff2e254d027fe1787c1f21d1c
南风以南 <25891297+nfyn@users.noreply.github.com>
2022-02-18 14:34:07 +0800

    添加rpc_client.py客户端启动代码

---
+++ rpc_client.py
@@ -0,0 +1,11 @@
+# coding: utf-8
+
+import time
+from rpc.rpc_client import RpcClient
+
+if __name__ == '__main__':
+    with RpcClient(host='localhost', port=15000) as client:
+        for i in range(100):
+            res = client.add(1, 14)
+            print(res)
+            time.sleep(3)
